de27c6c90fb6279ddfb81c7539e997ae27c5272a
nutchapon <nutchapon.han@gmail.com>
2019-09-25 11:09:23 +0700

    add code

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
+const saltRounds = 10;
+const myPlaintextPassword = 's0/\/\P4$$w0rD';
+const someOtherPlaintextPassword = 'not_bacon';
 const cors = require('cors');
 const knex = require('knex')
 
@@ -8,11 +11,12 @@ const db = knex({
   client: 'pg',
   connection: {
     host : '127.0.0.1',
-    user : 'aneagoie',
-    password : '',
-    database : 'smart-brain'
+    user : 'postgres',
+    password : 'postgres',
+    database : 'smartbrain'
   }
 });
+
 
 const app = express();
 
@@ -25,73 +29,77 @@ app.get('/', (req, res)=> {
 
 app.post('/signin', (req, res) => {
   db.select('email', 'hash').from('login')
-    .where('email', '=', req.body.email)
-    .then(data => {
-      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-      if (isValid) {
-        return db.select('*').from('users')
-          .where('email', '=', req.body.email)
-          .then(user => {
-            res.json(user[0])
-          })
-          .catch(err => res.status(400).json('unable to get user'))
-      } else {
-        res.status(400).json('wrong credentials')
-      }
-    })
-    .catch(err => res.status(400).json('wrong credentials'))
+  .where('email', '=', req.body.email)
+  .then(data => {
+    const isValid = bcrypt.compareSync(req.body.password, data[0].hash)
+    if (isValid) {
+      return db.select('*').from('users')
+        .where('email', '=', req.body.email)
+        .then(user => {
+          res.json(user[0])
+        })
+        .catch(err => res.status(400).json('cannot get user'))
+    } else {
+      res.status(400).json('recheck info')
+    }
+  })
+  .catch(err => res.status(400).json('wrong creadentails'))
 })
 
 app.post('/register', (req, res) => {
   const { email, name, password } = req.body;
-  const hash = bcrypt.hashSync(password);
+  let salt = bcrypt.genSaltSync(saltRounds);
+  const hash = bcrypt.hashSync(password, salt);
     db.transaction(trx => {
       trx.insert({
         hash: hash,
         email: email
       })
-      .into('login')
-      .returning('email')
-      .then(loginEmail => {
-        return trx('users')
-          .returning('*')
-          .insert({
-            email: loginEmail[0],
-            name: name,
-            joined: new Date()
-          })
-          .then(user => {
-            res.json(user[0]);
-          })
-      })
+        .into('login')
+        .returning('email')
+        .then(loginEmail => {
+          return trx('users')
+            .returning('*')
+            .insert({
+              email: loginEmail[0],
+              name: name,
+              joined: new Date()
+            })
+            .then(user => {
+              res.json(user[0])
+            })
+        })
       .then(trx.commit)
       .catch(trx.rollback)
     })
-    .catch(err => res.status(400).json('unable to register'))
+      .catch(err => res.status(400).json('cannot register'))
 })
 
 app.get('/profile/:id', (req, res) => {
   const { id } = req.params;
-  db.select('*').from('users').where({id})
+  db.select('*').from('users')
+    .where({
+      id: id
+    })
     .then(user => {
-      if (user.length) {
+      if (user.length){
         res.json(user[0])
       } else {
-        res.status(400).json('Not found')
+        res.status(400).json('not found users')
       }
-    })
-    .catch(err => res.status(400).json('error getting user'))
+  })
 })
+
 
 app.put('/image', (req, res) => {
   const { id } = req.body;
   db('users').where('id', '=', id)
-  .increment('entries', 1)
-  .returning('entries')
-  .then(entries => {
-    res.json(entries[0]);
-  })
-  .catch(err => res.status(400).json('unable to get entries'))
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+      res.json(entries[0])
+    })
+    .catch(err => res.status(400).json('cannot get entries'))
 })
 
 app.listen(3000, ()=> {
