61e086509b06010a1ca75b15d8d4b17c369b9aca
nutchapon <nutchapon.han@gmail.com>
2019-09-26 09:01:02 +0700

    adding port

--- README.md
+++ README.md
@@ -1,8 +1,3 @@
-# SmartBrain-api - v1
-Final project for Udemy course
+update 09/26/2019
 
-1. Clone this repo
-2. Run `npm install`
-3. Run `npm start`
-
-** Make sure you use postgreSQL instead of mySQL for this code base.
+this server is already update and wait to sync heroku
--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const saltRounds = 10;
-const myPlaintextPassword = 's0/\/\P4$$w0rD';
-const someOtherPlaintextPassword = 'not_bacon';
 const cors = require('cors');
-const knex = require('knex')
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
 
 const db = knex({
   client: 'pg',
@@ -27,81 +30,15 @@ app.get('/', (req, res)=> {
   res.send(database.users);
 })
 
-app.post('/signin', (req, res) => {
-  db.select('email', 'hash').from('login')
-  .where('email', '=', req.body.email)
-  .then(data => {
-    const isValid = bcrypt.compareSync(req.body.password, data[0].hash)
-    if (isValid) {
-      return db.select('*').from('users')
-        .where('email', '=', req.body.email)
-        .then(user => {
-          res.json(user[0])
-        })
-        .catch(err => res.status(400).json('cannot get user'))
-    } else {
-      res.status(400).json('recheck info')
-    }
-  })
-  .catch(err => res.status(400).json('wrong creadentails'))
-})
+app.post('/signin', (req,res) => {signin.handleSignin(req, res, db, bcrypt)})
 
-app.post('/register', (req, res) => {
-  const { email, name, password } = req.body;
-  let salt = bcrypt.genSaltSync(saltRounds);
-  const hash = bcrypt.hashSync(password, salt);
-    db.transaction(trx => {
-      trx.insert({
-        hash: hash,
-        email: email
-      })
-        .into('login')
-        .returning('email')
-        .then(loginEmail => {
-          return trx('users')
-            .returning('*')
-            .insert({
-              email: loginEmail[0],
-              name: name,
-              joined: new Date()
-            })
-            .then(user => {
-              res.json(user[0])
-            })
-        })
-      .then(trx.commit)
-      .catch(trx.rollback)
-    })
-      .catch(err => res.status(400).json('cannot register'))
-})
+app.post('/register', (req,res) => {register.handleRegister(req, res, db, bcrypt, saltRounds)})
 
-app.get('/profile/:id', (req, res) => {
-  const { id } = req.params;
-  db.select('*').from('users')
-    .where({
-      id: id
-    })
-    .then(user => {
-      if (user.length){
-        res.json(user[0])
-      } else {
-        res.status(400).json('not found users')
-      }
-  })
-})
+app.get('/profile/:id', (req, res) => {profile.handleProfile(req, res, db)})
 
+app.put('/image', (req, res) => {image.handleImage(req, res, db)})
+app.post('/imageUrl', (req, res) => {image.handleApiCall(req, res)})
 
-app.put('/image', (req, res) => {
-  const { id } = req.body;
-  db('users').where('id', '=', id)
-    .increment('entries', 1)
-    .returning('entries')
-    .then(entries => {
-      res.json(entries[0])
-    })
-    .catch(err => res.status(400).json('cannot get entries'))
-})
-
-app.listen(3000, ()=> {
-  console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, ()=> {
+  console.log(`app is running on port ${process.env.PORT}`);
 })
